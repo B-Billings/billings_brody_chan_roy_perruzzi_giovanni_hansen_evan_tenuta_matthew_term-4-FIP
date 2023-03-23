0564fb20bdddeaf1d8ffbbf6adef67c59b37ad22
Evan <39568306+Evanlyboy@users.noreply.github.com>
2023-03-23 11:11:28 -0400

    fixed the vue JS

--- js/components/projects.js
+++ js/components/projects.js
@@ -6,16 +6,19 @@ export default {
     template:
     `
         <section class="selector">
-        <div>
-            <img id="vueimg" :src='"images/" + item.ad_image' alt="proj image" @click="getitemInfo ^= true">
-        </div>
-            <div id="imagename">
-            <h3 id="undername">{{ item.ad_name }}</h3>
-        </div>
+        <div id="miniarea">
+                        <img id="vueimg" :src='"images/" + ad_image' alt="proj image" @click="getitemInfo ^= true">
+             </div>
+                        <div id="imagename">
+                            <h3 id="undername">{{ item.ad_name }}</h3>
+            </div>
         <div id="projectcont" v-show="getitemInfo">
         <div class="projinfo">
-        <h3> About </h3>
-            <p id="desc">{{ item.ad_desc }}</p>
+        <h3> Engine </h3>
+       
+            <p id="desc">{{ ad_name }}</p>
+            <hr>
+        <h3>Horsepower</h3>
         </div>
     </div>
         </section>
@@ -23,7 +26,8 @@ export default {
 
     data() {
         return {
-            getitemInfo: false
+            getitemInfo: true
         }
     }
 }
+
--- js/main.js
+++ js/main.js
@@ -1,44 +1,37 @@
 import portStuff from './components/projects.js';
 import portpiece from './components/pieces.js';
 import { SendMail } from "./components/mailer.js";
+
 (() => {
     const { createApp } = Vue
-
-    createApp({
-        created() {
-            console.log('vue instance is created');
-
-            //fetch('./scripts/json.php')
-
-            fetch('./data.json')
-                .then(res => res.json())
-                .then(data => this.ProjectData = data)
-            .catch(error => console.error(error));
-        },
-
-        data() {
-            console.log('vue data check');
-            return {
-                ProjectData: {},
-                displayothers: true,
-                getitemInfo: false
-            }
-        },
-        
-        components: {
-            portstuff: portStuff,
-            portpiece: portpiece
-        }
-    }).mount('#app')
-
+    
     createApp({
         data() {
             return {
+                adData: [],
                 message: 'Test!'
             }
         },
 
         methods: {
+
+            // Protected method that runs on page load
+            mounted() {
+                this.getDataFromAPI('items');
+            },
+
+            // Here is where we get the data from Lumen
+            getDataFromAPI(requestedData) {
+                fetch(`http://localhost:8888/brodyFork/lumen/public/ads`)
+                  .then(response => response.json())
+                  .then(data => {
+                    this.dataFromAPI = data;
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  });
+            },
+
             processMailFailure(result) {
         
             },
@@ -52,8 +45,12 @@ import { SendMail } from "./components/mailer.js";
                     .then(data => this.processMailSuccess(data))
                     .catch(err => this.processMailFailure(err));
             }
-        }
-    }).mount('#mail-form')
+        },
 
-    
+        components: {
+            portstuff: portStuff,
+            portpiece: portpiece
+        }
+    })
+    .mount('#app, #mail-form')
 })()
