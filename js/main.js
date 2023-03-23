2acba8bf2f5212a000df7cb5a6722da5fba0ee1a
Evan <39568306+Evanlyboy@users.noreply.github.com>
2023-03-23 11:47:43 -0400

    ok, the JS is fixed. Now I have to connect the vue on html to the database request

--- js/main.js
+++ js/main.js
@@ -1,44 +1,39 @@
 import portStuff from './components/projects.js';
 import portpiece from './components/pieces.js';
 import { SendMail } from "./components/mailer.js";
+
 (() => {
     const { createApp } = Vue
-
-    createApp({
-        created() {
-            console.log('vue instance is created');
-
-            //fetch('./scripts/json.php')
-
-            fetch('./data.json')
-                .then(res => res.json())
-                .then(data => this.ProjectData = data)
-            .catch(error => console.error(error));
-        },
-
-        data() {
-            console.log('vue data check');
-            return {
-                ProjectData: {},
-                displayothers: true,
-                getitemInfo: false
-            }
-        },
-        
-        components: {
-            portstuff: portStuff,
-            portpiece: portpiece
-        }
-    }).mount('#app')
-
+    
     createApp({
         data() {
             return {
+                adData: [],
                 message: 'Test!'
             }
         },
 
         methods: {
+
+            // Protected method that runs on page load
+            mounted() {
+                // We're calling this method on page load, and passing in the 'items' variable
+                this.getDataFromAPI('items');
+            },
+
+            // Here is where we get the data from Lumen
+            getDataFromAPI(requestedData) {
+                fetch(`http://localhost:8888/brodyFork/lumen/public/ads`)
+                  .then(response => response.json())
+                  .then(data => {
+                    // Then we can make adData the data we pull from the database
+                    this.adData = data;
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  });
+            },
+
             processMailFailure(result) {
         
             },
@@ -52,8 +47,12 @@ import { SendMail } from "./components/mailer.js";
                     .then(data => this.processMailSuccess(data))
                     .catch(err => this.processMailFailure(err));
             }
-        }
-    }).mount('#mail-form')
+        },
 
-    
+        components: {
+            portstuff: portStuff,
+            portpiece: portpiece
+        }
+    })
+    .mount('#app, #mail-form')
 })()
