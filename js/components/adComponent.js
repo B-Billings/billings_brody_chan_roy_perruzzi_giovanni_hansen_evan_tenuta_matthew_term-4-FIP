7ce879b594b6e5cffedebfeb73b8433661ccf342
Evan <39568306+Evanlyboy@users.noreply.github.com>
2023-03-24 00:41:17 -0400

    styled promo content page

--- js/components/adComponent.js
+++ js/components/adComponent.js
@@ -7,20 +7,16 @@ export default {
     
     template:
     `
-        <section class="">
-        <div>
+        <div class="gridMiddleStack">
             <img class="vueImg" :src='"images/" + item.ad_image' alt="proj image" @click="getitemInfo ^= true">
+            <div class="imageName"><h3 class="undername">{{ item.ad_name }}</h3></div>
+            <div class="projectCont" v-show="getitemInfo">
+                <div class="projinfo">
+                <h3> About </h3>
+                <p id="desc">{{ item.ad_desc }}</p>
+                </div>
+            </div>
         </div>
-            <div class="imageName">
-            <h3 class="undername">{{ item.ad_name }}</h3>
-        </div>
-        <div class="projectCont" v-show="getitemInfo">
-        <div class="projinfo">
-        <h3> About </h3>
-        <p id="desc">{{ item.ad_desc }}</p>
-        </div>
-    </div>
-        </section>
     `,
 
     data() {
