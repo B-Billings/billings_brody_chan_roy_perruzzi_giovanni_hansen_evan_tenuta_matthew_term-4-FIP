032269ddfd7ae566f99e99d722e2eababaf1ae29
GiovanniP24 <delmert38@gmail.com>
2023-04-06 04:37:57 -0400

    dev.gp.UpdatingPopUp

--- js/popup.js
+++ js/popup.js
@@ -1,44 +1,50 @@
-var popupLink = document.getElementById("popup-link");
-var popupBox = document.getElementById("popup-box");
-var popupOverlay = document.getElementById("popup-overlay");
-var popupClose = document.getElementById("popup-close");
-var form = document.getElementById("subscribe-form");
+const popupLink = document.getElementById("popup-link");
+const popupBox = document.getElementById("popup-box");
+const popupOverlay = document.getElementById("popup-overlay");
+const popupClose = document.getElementById("popup-close");
+const form = document.getElementById("subscribe-form");
 
-var subscribed = sessionStorage.getItem('subscribed');
+const subscribed = sessionStorage.getItem('subscribed');
 if (!subscribed) {
-    popupLink.addEventListener("click", function (event) {
+    popupLink.addEventListener("click", event => {
         event.preventDefault();
         popupBox.style.display = "block";
         popupOverlay.style.display = "block";
     });
 }
 
-popupClose.addEventListener("click", function () {
+popupClose.addEventListener("click", () => {
     popupBox.style.display = "none";
     popupOverlay.style.display = "none";
 });
 
-popupOverlay.addEventListener("click", function () {
+popupOverlay.addEventListener("click", () => {
     popupBox.style.display = "none";
     popupOverlay.style.display = "none";
 });
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", event => {
     event.preventDefault();
 
-    var formData = new FormData(form);
+    const formData = new FormData(form);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.getAttribute("action"));
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            alert("Thank you for subscribing!");
-            sessionStorage.setItem('subscribed', true);
-            setTimeout(function () {
+    fetch('lumen/public/subscribers', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                sessionStorage.setItem('subscribed', true);
                 popupBox.style.display = "none";
-                popupOverlay.style.display = "none";
-            }, 2000);
-        }
-    };
-    xhr.send(formData);
+                const successMessage = document.getElementById("subscribe-modal");
+                successMessage.style.display = "block";
+                form.reset();
+                setTimeout(() => {
+                    successMessage.style.display = "none";
+                }, 3000);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
